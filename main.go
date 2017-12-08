3c122da8cdedcb53435cf32e63af2aafeeb07777
Jules <julio.telan@shoresuite.com>
2017-12-08 12:59:59 +0800

    Working setup routing

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
@@ -19,5 +21,6 @@ func main() {
 		return
 	}
 
-	fmt.Println("Hello.")
+	router := NewRouter()
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
